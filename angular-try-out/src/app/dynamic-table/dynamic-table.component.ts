1d51413198aedee53d7d272609baba568fef9e0a
Elena Smeu <elena.smeu@skyspecs.com>
2024-06-11 20:57:36 +0200

    dynamic sorting done

--- angular-try-out/src/app/dynamic-table/dynamic-table.component.ts
+++ angular-try-out/src/app/dynamic-table/dynamic-table.component.ts
@@ -15,19 +15,15 @@ type SortDirection = "Asc" | "Desc" | "None";
     <thead class="py-10 bg-gray-100 rounded-t">
       <tr>
         <th *ngFor="let col of columns">
-          <div class="text-3xl text-gray-800 flex flex-row justify-center items-center"> 
-            <div> {{ col.name | capitalize : "titleCase" }} </div>
-            <div class="flex flex-row pl-2 justify-center cursor-pointer">
-            <div [ngClass]="{'text-red-800' : col.sort =='Asc'}"  class="text-sm pr-1" (click)="applySortBy(col.name, 'Asc')"> <fa-icon [icon]="iconUp"></fa-icon> </div>
-            <div [ngClass]="{'text-red-800' : col.sort =='Desc'}"  class="text-sm" (click)="applySortBy(col.name, 'Desc')"> <fa-icon [icon]="iconDown"></fa-icon> </div>
-          </div>
+          <div class="text-3xl text-gray-800 flex flex-row justify-center items-center cursor-pointer"> 
+            <div (click)="applySortBy(col)"> {{ col | capitalize : "titleCase" }} </div>
           </div>
         </th>
       </tr>
     </thead>
     <tbody>
       <tr class="border border-l-1 text-center text-lg" *ngFor="let row of data" >
-        <td *ngFor="let col of columns" class="p-2" >{{ row[col.name] }}</td>
+        <td *ngFor="let col of columns" class="p-2" >{{ row[col] }}</td>
       </tr>
     </tbody>
   </table>
@@ -37,25 +33,50 @@ export class DynamicTableComponent implements OnInit {
   @Input({required: true}) data: any[] = [];
   iconUp = faArrowDownLong
   iconDown = faArrowUpLong
-  columns : {name: string, sort: SortDirection}[]  = []
+  columns :  string[]  = []
+  sortby: string = ""
+  sortDirection: SortDirection = "Asc"
 
   ngOnInit() {
     this.columns = this.processColumns();
 
   }
   
-  processColumns(): {name: string, sort: SortDirection}[] {
+  processColumns(): string[] {
    if(!this.data || this.data.length === 0) {
     return [];
    }
-   return Array.from(new Set<string>(this.data.flatMap(Object.keys))).map(key => ({name: key, sort: "None"}))
+   return Array.from(new Set<string>(this.data.flatMap(Object.keys)));
+  }
+  getNextSortDirection(isSameColumn: boolean): SortDirection {
+    if (isSameColumn) {
+      switch(this.sortDirection) {
+        case "Asc" : return "Desc"
+        default: return "Asc"
+      }
+    } else {
+     return "Asc"
+    }
   }
 
-  applySortBy(col: string, sortType: SortDirection): void {
-    this.columns = this.columns.map(({name}) => {
-     return  name == col ? {name, sort: sortType } : {name, sort : "None"}
-    });
-    console.log(col, sortType);
+  applySortBy(col: string): void {
+   this.sortDirection = this.getNextSortDirection(this.sortby == col)
+   this.sortby = col;
+   this.data.sort((a, b) => {
+      const compareTermA = a[col]
+      const compareTermB = b[col]
+      switch (typeof compareTermA) 
+      {
+        case 'string': return compareTermA.localeCompare(compareTermB)
+        case 'number': return compareTermA > compareTermB
+        default: return compareTermA
+      }
+
+  })
+    if(this.sortDirection == "Asc") {
+      return
+    }
+    this.data.reverse();
   }
 
 
